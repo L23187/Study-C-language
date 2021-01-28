005c706fb9eb575b0c8d2ab31ae6a3ccc0c7f9bb
L2135 <1250405509@qq.com>
2021-01-29 01:36:38 +0800

    仅供自己大学期间学习C语言使用

---
+++ 整数的四则运算.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+	int m,n;
+	double c;
+	scanf("%d%d",&m,&n);
+	printf("%d + %d = %d\n",m,n,m+n);
+	printf("%d - %d = %d\n",m,n,m-n);
+	printf("%d * %d = %d\n",m,n,m*n);
+	printf("%d / %d = %d\n",m,n,m/n);
+	return 0;
+} 
