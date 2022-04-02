66e4fb6e4c36598359a342c7ed74197109428d2f
建伟 刘 <1250405509@qq.com>
2022-04-02 21:38:32 +0800

    学习学习

--- 22年天梯备赛/个位数统计/个位数统计/个位数统计.cpp
+++ 22年天梯备赛/个位数统计/个位数统计/个位数统计.cpp
@@ -1,6 +1,16 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
 int main(void) {
-	int 
+	int n;
+	scanf("%d", &n);
+	int a[10];
+	int x = 0;
+	for (int i = 0; i < 1000; i++) {
+		x = n % 10;
+		n = n / 10;
+		if (n == 0)
+			break;
+		printf("%d\n", x);
+	}
 	return 0;
 }
