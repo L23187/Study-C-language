d7bc94395b9d0f150e9d5914678fb4307373e06e
建伟 刘 <1250405509@qq.com>
2022-04-06 14:14:48 +0800

    测试

--- 22年天梯备赛/个位数统计/个位数统计/个位数统计.cpp
+++ 22年天梯备赛/个位数统计/个位数统计/个位数统计.cpp
@@ -1,16 +1,33 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
+#include<string.h>
 int main(void) {
-	int n;
-	scanf("%d", &n);
-	int a[10];
-	int x = 0;
-	for (int i = 0; i < 1000; i++) {
-		x = n % 10;
-		n = n / 10;
-		if (n == 0)
-			break;
-		printf("%d\n", x);
+	//设一个存放1000位数字的字符串
+	char a[1001];
+	scanf("%s", a);
+	int k = 1;
+	//将字符串的数从小到大排列，并再存储在一个整型数组中
+	for (int i = 0; i < strlen(a) - 1; i++) {
+		for (int j = 0; j < strlen(a) - 1 - i; j++) {
+			if (a[j] - '0' > a[j + 1] - '0') {
+				char num = a[j];
+				a[j] = a[j + 1];
+				a[j + 1] = num;
+			}
+		}
+	}
+	int s[1001];
+	for (int i = 0; i < strlen(a); i++) {
+		s[i] = a[i] - '0';
+	}
+	for (int i = 0; i < strlen(a); i++) {
+		if (s[i] == s[i + 1]) {
+			k++;
+		}
+		if (s[i] != s[i + 1]) {
+			printf("%d:%d\n", s[i],k);
+			k = 1;
+		}
 	}
 	return 0;
 }
