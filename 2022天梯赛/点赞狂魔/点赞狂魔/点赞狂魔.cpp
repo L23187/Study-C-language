59c35ffccf457c1a75bd68e6dbb9e1173c806023
建伟 刘 <1250405509@qq.com>
2022-04-21 15:33:51 +0800

    学习学习

---
+++ 2022天梯赛/点赞狂魔/点赞狂魔/点赞狂魔.cpp
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main(void) {
+	int n, k;
+	scanf("%d", &n);
+	char name[9];
+	int x;
+	for (int i = 0; i < n; i++) {
+		scanf("%s", name);
+		scanf("%d", &k);
+		for (int j = 0; j < k; j++) {
+			
+		}
+	}
+	return 0;
+}
--- 2022天梯赛/盲盒包装流水线/盲盒包装流水线/盲盒包装流水线.cpp
+++ 2022天梯赛/盲盒包装流水线/盲盒包装流水线/盲盒包装流水线.cpp
@@ -1,47 +1,43 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
-void find(int n, int* p) {
-	for (int i = 0; i < 100001; i++) {
-		if (p[n] > 0) {
-			printf("%d\n", p[n]);
-			break;
+void output(int n, int* p) {
+	printf("%d\n",p[n]);
+}
+int find(int k, int* num, int n, int flag,int number) {
+	for (int j = 0; j < n; j++) {
+		if (number == num[j]) {
+			return 1;
 		}
 	}
+	return 0;
 }
 int main(void) {
 	int n, s, k;
 	scanf("%d %d", &n, &s);
-	int num[100001];//存储盒子的序号
+	int num[1001];//存储盒子的序号
 	//输入盒子编号
 	for (int i = 0; i < n; i++) {
 		scanf("%d", &num[i]);
 	}
-	//
-	int hz[100], x;
+	int nw;
 	int p[100001];
-	for (int i = 0; i < n / s; i++) {
+	int t;
+	for (int i = 1; i <= n / s; i++) {
+		t = s * i  - 1;
 		for (int j = 0; j < s; j++) {
-			scanf("%d", &hz[j]);
-			x = s - 1;
-			p[num[s - 1]] = hz[j];
-			x--;
+			scanf("%d", &nw);
+			p[num[t]] = nw;
+			t--;
 		}
 	}
-
 	//输入查找的次数
 	scanf("%d", &k);
 	int number,flag = 0;
 	for (int i = 0; i < k; i++) {
 		flag = 0;
-		scanf("%d\n", &number);
-		for (int j = 0; j < n; j++) {
-			if (number == num[j]) {
-				flag = 1;
-				break;
-			}
-		}
-		if (flag) {
-			find(number, p);
+		scanf("%d", &number);
+		if (find(k, num, n, flag, number)) {
+			output(number, p);
 		}
 		else {
 			printf("Wrong Number\n");
